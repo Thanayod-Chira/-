1a6f8db77aa84ca5240a2781ab8cb2f26919b43c
Thanayod <thanayod.chira@gmail.com>
2025-08-07 16:05:07 +0700

    Update Project_2.py

--- Project_2.py
+++ Project_2.py
@@ -5,17 +5,15 @@ class Product:
 
 class Store:
     def __init__(self):
-        self.__products = []  # เก็บสินค้าแบบ private list
+        self.__products = []
 
     def add_product(self, name, quantity):
-        new_product = Product(name, quantity)  # สร้างสินค้าใหม่
-        self.__products.append(new_product)    # เพิ่มเข้าไปใน list
+        new_product = Product(name, quantity)
+        self.__products.append(new_product)
 
     def show_products(self):
         for product in self.__products:
             print(f"สินค้า: {product.name}, จำนวน: {product.quantity}")
-
-# --- ตัวอย่างการทดสอบโปรแกรม ---
 my_store = Store()
 my_store.add_product("Laptop", 15)
 my_store.add_product("Mouse", 50)
